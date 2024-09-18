0d87818097d9b7a6339b8a6e30df495971ebd7b4
Nathy <maizanatiele@outlook.com>
2024-09-18 14:24:25 -0300

    Create README.md.py
    Basic Codes in Python
    Códigos básicos em Python

---
+++ README.md.py
@@ -0,0 +1,36 @@
+#cauculos sobre lucros.
+faturamento=2000.85
+custo=900
+reposição=150
+tecerizaçao=200
+mensagem= "As vendas formam um sucesso!"
+
+print("faturamento", faturamento)
+print("custo", custo)
+print("reposição", reposição)
+print("lucro", faturamento - custo + reposição - tecerizaçao)
+print( mensagem)
+
+#Dia,mês e ano.
+
+mng=
+dia=input('Qual foi o dia em que você nasceu?')
+mes=input('Qual o mês em que você nasecu?')
+ano=input('qual foi o ano em que você nasceu?')
+print('voce naseu no dia',dia)
+print('do mês',mes)
+print('do ano de',ano)
+
+#Somas dos valores(calculadora)
+
+n1=int(input('Digite um numero:'))
+n2=int(input('Digite outro numero:'))
+s=n1 + n2
+print('a soma entre {} e {} equivale a {}'.format(n1,n2,s))
+print('soma dos valoror={}'.format(n1+n2))
+
+
+
+
+
+
